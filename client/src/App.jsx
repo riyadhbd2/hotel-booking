64e5eb4534230f545a6c32c977405e3467a558a5
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-04-28 16:37:28 +0200

    hero section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
-  return (<div>
-    {!isOwnerPath && <Navbar />}
-    </div>);
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
